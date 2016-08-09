904860da07ce682c76ef657ea852b306629d69c9
Kusum Saini <kusumsaini@gmail.com>
2016-08-09 17:50:12 +0530

    Added missing rmdir menthod

--- ComposerScript/Installer.php
+++ ComposerScript/Installer.php
@@ -74,4 +74,27 @@ class Installer
             copy($src, $dst);
         }
     }
+    
+    private static function rrmdir($dir) {
+        if (!file_exists($dir)) {
+            return true;
+        }
+    
+        if (!is_dir($dir)) {
+            return unlink($dir);
+        }
+    
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+    
+            if (!deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+                return false;
+            }
+    
+        }
+    
+        return rmdir($dir);
+    }
 }
